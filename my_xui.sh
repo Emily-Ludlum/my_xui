b9bbad25f9f661eb20b9fd28c7ecc7b3867a47d9
Emily-Ludlum <160514488+Emily-Ludlum@users.noreply.github.com>
2024-04-19 08:09:17 -0500

    my_xui.sh

--- my_xui.sh
+++ my_xui.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 默认设置
-DEFAULT_DOMAIN="xui.vvps.link"
-DEFAULT_EMAIL="xui@vvps.link"
+DEFAULT_DOMAIN="example.com"
+DEFAULT_EMAIL="your_email@example.com"
 
 # 询问用户输入域名和邮箱地址
 read -p "请输入要申请 SSL 证书的域名（默认值：$DEFAULT_DOMAIN）: " DOMAIN
@@ -30,12 +30,8 @@ apt install -y wget curl socat cron
 
 # 安装 Warp
 echo "安装 Warp..."
-wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<EOF
-2
-1
-1
-3
-EOF
+wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh -y
+
 
 # 安装签发证书工具
 echo "安装签发证书工具..."
@@ -47,16 +43,25 @@ echo "申请 SSL 证书，并下载到服务器目录..."
 ~/.acme.sh/acme.sh --issue -d $DOMAIN --standalone --listen-v6
 ~/.acme.sh/acme.sh --installcert -d $DOMAIN --key-file /root/private.key --fullchain-file /root/cert.crt
 
-# 安装 X-UI
+# 安装 expect 工具
+apt install -y expect
+
+# 安装 X-UI 的命令之前添加以下代码
 echo "正在安装 X-UI..."
-wget -N --no-check-certificate https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh && bash install.sh
 
-# 在 install.sh 脚本中添加参数接收和使用的逻辑
-sed -i 's/^read -p "Enter username for x-ui: " USERNAME$/USERNAME="VIP"/' install.sh
-sed -i 's/^read -p "Enter password for x-ui: " PASSWORD$/PASSWORD="My@123123"/' install.sh
-sed -i 's/^read -p "Enter port for x-ui: " PORT$/PORT="8880"/' install.sh
-
-bash install.sh
+# 使用 expect 来自动填入账户名、密码和端口
+echo "正在设置 X-UI 的用户名、密码和端口..."
+expect << EOF
+spawn bash /usr/local/XrayR/install.sh
+expect "设置您的账户名:"
+send "VIP\n"
+expect "请设置您的账户密码:"
+send "My@123123\n"
+expect "请设置面板访问端口:"
+send "8880\n"
+expect eof
+EOF
 
 
 # 提示用户设置 X-UI 的用户名、密码和端口
